e8be97157053eeff4f31555a6ec085940bb6b1a3
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-04-16 11:24:09 -0430

    Add chosen plugin.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
+//= require chosen.jquery.min
+//= require bootstrap
 //= require_tree .
