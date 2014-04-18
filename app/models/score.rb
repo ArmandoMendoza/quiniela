c6b49ea2d5bf4c6b50b68e4cbe7900dd31286cf9
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-04-18 01:06:28 -0430

    Fix seed data. Add to_s method to Score. Add the name of teams in form of score.

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,4 +3,9 @@ class Score < ActiveRecord::Base
   validates_presence_of :visitor, :local, :match_time
 
   MATCH_TIMES = { "Partido" => "full-time", "Prorroga" => "aggregate", "Penales" => "penalties" }
+
+  def to_s
+    "#{local}-#{visitor}"
+  end
+
 end
