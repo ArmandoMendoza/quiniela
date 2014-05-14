2522d380fa768b31b85fa09def0698563e01cf28
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-05-13 20:20:48 -0430

    Add improvements in dashboard. Implemented the method to generate classification table

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,7 @@ class DashboardController < ApplicationController
       @pool = current_user.last_active_pool
       @matches = @pool.matches.includes(:group).by_date(Date.today)
       @bets = @pool.bets.with_matches_by_date(Date.today)
+      @users = @pool.uniq_users
     end
   end
 end
