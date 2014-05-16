110429befe8ff17b6caabea8ff4e34e1160b37ff
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-05-15 23:56:28 -0430

    Add the image of flags of teams.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def apply_colors
     cycle("rojo", "naranja", "amarillo", "verde", "celeste", "azul", "fucsia", "morado")
   end
+
+  def flag(team)
+    name = team.name.downcase.gsub(" ","_")
+    image_tag "flags/33x22_#{name}.png"
+  end
+
+  def big_flag(team)
+    name = team.name.downcase.gsub(" ","_")
+    image_tag "flags/50x33_#{name}.png"
+  end
 end
