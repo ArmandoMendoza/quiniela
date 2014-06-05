40e083741565e2fbe866f59bf2b5cce21cc273cf
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-06-04 23:19:55 -0430

    fix helper highligth_current_user

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,6 +1,2 @@
 module DashboardHelper
-
-  def highlight_current_user(value, method)
-    "highlight" if current_user.send(method) == value
-  end
 end
--- app/helpers/pools_helper.rb
+++ app/helpers/pools_helper.rb
@@ -1,2 +1,5 @@
 module PoolsHelper
+  def highlight_current_user(value, method)
+    "highlight" if current_user.send(method) == value
+  end
 end
