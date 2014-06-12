6f1eae9890792a569dd2af539253917b2b397927
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-06-12 09:03:15 -0430

    Added options for format of view the bets of user.

--- app/helpers/pools_helper.rb
+++ app/helpers/pools_helper.rb
@@ -2,4 +2,10 @@ module PoolsHelper
   def highlight_current_user(value, method)
     "highlight" if current_user.send(method) == value
   end
+
+  def my_bets_button
+    link_to "#", class: "btn btn-default dropdown-toggle", data: { toggle: "dropdown" } do
+      raw("Ver mis Apuestas ") + content_tag(:span, nil, class: "caret")
+    end
+  end
 end
