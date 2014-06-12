ff06c787d4f3d91aacf63294ad2ebdc62eee38e1
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-06-11 21:44:46 -0430

    Fixed bugs.

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,5 +1,6 @@
 class PoolsController < ApplicationController
   before_action :set_pool, except: [:index, :new, :create]
+  before_action :check_status_of_pool, only: :bets
   authorize_resource
 
   def index
@@ -8,8 +9,8 @@ class PoolsController < ApplicationController
 
   def show
     if current_user.regular?
-      @matches = @pool.matches.includes(:group).by_date(Date.today)
-      @bets = current_user.bets_in_pool_with_date(@pool, Date.today)
+      @matches = @pool.matches.includes(:group).by_date(Date.current)
+      @bets = current_user.bets_in_pool_with_date(@pool, Date.current)
       @users = @pool.users
       @table = @pool.users_classification
     end
@@ -60,5 +61,10 @@ class PoolsController < ApplicationController
 
     def pool_params
       params[:pool].permit!
+    end
+
+    def check_status_of_pool
+      redirect_to(pool_path(@pool),
+        notice: "El tiempo para completar tus pronosticos ha terminado") if @pool.stopped
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140613205141) do
+ActiveRecord::Schema.define(version: 20140613205142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20140613205141) do
     t.datetime "updated_at"
     t.integer  "price"
     t.float    "pot_percentage"
+    t.boolean  "stopped",        default: false
   end
 
   create_table "registrations", force: true do |t|
