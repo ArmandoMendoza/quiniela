4d8fc8989dad666e3842c7999be94d6ce91f4fc5
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-06-14 17:42:40 -0430

    Fix on search form of dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,11 @@
 class DashboardController < ApplicationController
   def index
+    @q = Bet.search(params[:q])
     @pools = current_user.pools
     @matches = Match.all
     if params[:q].present?
-      @q = Bet.search(params[:q])
       @bets = @q.result.includes({ match: :group }, :pool, :user)
     else
-      @q = Bet.search(params[:q])
       @bets = Bet.where(user_id: current_user.id).includes({match: :group}, :pool, :user).limit(10)
     end
   end
