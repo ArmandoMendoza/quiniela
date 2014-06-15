5c5c8f034bec106f16385c62a57e10f622ae7c2b
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-06-14 19:46:53 -0430

    Fixes in dashboard and reports of bets.

--- app/pdfs/all_bets_pdf.rb
+++ app/pdfs/all_bets_pdf.rb
@@ -9,7 +9,7 @@ class AllBetsPdf < Prawn::Document
 
   def header(user)
     font_size(20) { text @pool.name, align: :center, style: :bold }
-    draw_text "Jugador:  #{user.full_name}", at: [0,680]
+    draw_text "Jugador:  #{user.full_name} - #{user.nickname}", at: [0,680]
     draw_text "Fecha: #{Time.now.strftime("%d-%m-%Y  %H:%M")}", at: [320, 680]
     draw_text "Goleador del Mundial: #{user.answers.on_pool(@pool).player}", at: [0,660]
     draw_text "Equipo Ganador: #{user.answers.on_pool(@pool).team}", at: [320,660]
