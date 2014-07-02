9b38d0724c8999b394c5663bc907af202dea7a7e
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-07-02 16:52:59 -0600

    Added search form to elimination bets in dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,21 @@
 class DashboardController < ApplicationController
   def index
-    @q = Bet.search(params[:q])
+    @q = EliminationBet.search(params[:q])
     @pools = current_user.pools
-    @matches = Match.all
+    @matches = EliminationMatch.all
     if params[:q].present?
-      @bets = @q.result.includes({ match: :group }, :pool, :user)
+      @bets = @q.result.includes(:elimination_match, :pool, :user)
     else
-      @bets = Bet.where(user_id: current_user.id).includes({match: :group}, :pool, :user).limit(10)
+      @bets = []
     end
   end
 end
+
+    # @q = Bet.search(params[:q])
+    # @pools = current_user.pools
+    # @matches = Match.all
+    # if params[:q].present?
+    #   @bets = @q.result.includes({ match: :group }, :pool, :user)
+    # else
+    #   @bets = Bet.where(user_id: current_user.id).includes({match: :group}, :pool, :user).limit(10)
+    # end
