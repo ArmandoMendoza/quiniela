2e2dc8dada10029de3b856d299ef1e73e853d550
Armando Mendoza <fcoarmandomendoza@gmail.com>
2018-06-15 13:21:56 -0400

    Minor fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,21 +1,22 @@
 class DashboardController < ApplicationController
   def index
-    @q = EliminationBet.search(params[:q])
+    @q = Bet.search(params[:q])
     @pools = current_user.pools
-    @matches = EliminationMatch.all
+    @matches = Match.all
     if params[:q].present?
-      @bets = @q.result.includes(:elimination_match, :pool, :user)
+      @bets = @q.result.includes({ match: :group }, :pool, :user)
     else
-      @bets = []
+      @bets = Bet.where(user_id: current_user.id).includes({match: :group}, :pool, :user).limit(10)
     end
+    
+    # @q = EliminationBet.search(params[:q])
+    # @pools = current_user.pools
+    # @matches = EliminationMatch.all
+    # if params[:q].present?
+    #   @bets = @q.result.includes(:elimination_match, :pool, :user)
+    # else
+    #   @bets = []
+    # end
   end
 end
 
-    # @q = Bet.search(params[:q])
-    # @pools = current_user.pools
-    # @matches = Match.all
-    # if params[:q].present?
-    #   @bets = @q.result.includes({ match: :group }, :pool, :user)
-    # else
-    #   @bets = Bet.where(user_id: current_user.id).includes({match: :group}, :pool, :user).limit(10)
-    # end
